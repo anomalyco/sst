3ebb8ed41539df3bfef2eb2d53e5afecc755fe6d
Des-Angelo Pennant <desangelo@particularpresence.com>
2025-12-11 16:45:46 -0500

    fix(vpc): add missing parent parameter to EipAssociation in createNatInstances

--- platform/src/components/aws/vpc.ts
+++ platform/src/components/aws/vpc.ts
@@ -1018,10 +1018,13 @@ export class Vpc extends Component implements Link.Linkable {
               ),
             );
 
-            new ec2.EipAssociation(`${name}NatInstanceEipAssociation${i + 1}`, {
+            new ec2.EipAssociation(`${name}NatInstanceEipAssociation${i + 1}`, 
+            {
               instanceId: instance.id,
               allocationId: elasticIps[i]?.id ?? nat.ip![i],
-            });
+            },
+            { parent: self }
+          );
 
             return instance;
           }),
