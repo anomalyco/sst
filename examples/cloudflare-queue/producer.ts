adadc9d1ef3a0dded8816a654369f02e1cdb33ae
Bachitter <143877686+bachiitter@users.noreply.github.com>
2026-02-01 12:45:44 -0800

    fix example to use Resource instead of env

--- examples/cloudflare-queue/producer.ts
+++ examples/cloudflare-queue/producer.ts
@@ -1,8 +1,8 @@
 import { Resource } from "sst";
 
 export default {
-  async fetch(request, env) {
-    await env.MyQueue.send({ hello: "world" });
+  async fetch() {
+    await Resource.MyQueue.send({ hello: "world" });
     return new Response("Message sent!");
   },
 };
