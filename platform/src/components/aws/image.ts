e8a69fa278a54f5fe589b9c8921ea11523deb230
Meng Lin <menglinmaker@gmail.com>
2026-03-03 22:57:13 +1100

    extract Platform type which could not be processed by doc generator

--- platform/src/components/aws/image.ts
+++ platform/src/components/aws/image.ts
@@ -7,11 +7,13 @@ import { ecr } from "@pulumi/aws";
 import { Semaphore } from "../../util/semaphore.js";
 import { bootstrap } from "./helpers/bootstrap.js";
 import {
-  Platform,
   Image as PulumiDockerImage,
   ImageArgs as PulumiDockerImageArgs,
 } from "@pulumi/docker-build";
 import path from "path";
+
+// Extracted from `@pulumi/docker-build` `Platform` - unhandled by doc generator.
+export type Platform = "darwin/386" | "darwin/amd64" | "darwin/arm" | "darwin/arm64" | "dragonfly/amd64" | "freebsd/386" | "freebsd/amd64" | "freebsd/arm" | "linux/386" | "linux/amd64" | "linux/arm" | "linux/arm64" | "linux/mips64" | "linux/mips64le" | "linux/ppc64le" | "linux/riscv64" | "linux/s390x" | "netbsd/386" | "netbsd/amd64" | "netbsd/arm" | "openbsd/386" | "openbsd/amd64" | "openbsd/arm" | "plan9/386" | "plan9/amd64" | "solaris/amd64" | "windows/386" | "windows/amd64"
 
 export interface ImageArgs {
   /**
@@ -56,7 +58,7 @@ export interface ImageArgs {
   /**
    * Set target platform(s) for the build. Defaults to the host's platform.
    *
-   * Equivalent to Docker's `--platform` flag.
+   * Equivalent to Docker's `--platform` flag. 
    */
   platforms?: Input<Platform[]>;
   /**
@@ -109,6 +111,185 @@ const limiter = new Semaphore(
   parseInt(process.env.SST_BUILD_CONCURRENCY_CONTAINER || "1"),
 );
 
+/**
+ * The `Image` component builds docker images and uploads them to [AWS ECR (Elastic Container Registry)](https://aws.amazon.com/ecr/).
+ *
+ * #### Supported runtimes
+ *
+ * Currently supports **Node.js** and **Golang** functions. **Python** and **Rust**
+ * are community supported. Other runtimes are on the roadmap.
+ *
+ * @example
+ *
+ * #### Minimal example
+ *
+ *
+ * <Tabs>
+ *   <TabItem label="Node">
+ *   Pass in the path to your handler function.
+ *
+ *   ```ts title="sst.config.ts"
+ *   new sst.aws.Function("MyFunction", {
+ *     handler: "src/lambda.handler"
+ *   });
+ *   ```
+ *
+ *   [Learn more below](#handler).
+ *   </TabItem>
+ *   <TabItem label="Python">
+ *   Pass in the path to your handler function.
+ *
+ *   ```ts title="sst.config.ts"
+ *   new sst.aws.Function("MyFunction", {
+ *     runtime: "python3.11",
+ *     handler: "functions/src/functions/api.handler"
+ *   });
+ *   ```
+ *
+ *   You need to have uv installed and your handler function needs to be in a uv workspace. [Learn more below](#handler).
+ *   </TabItem>
+ *   <TabItem label="Go">
+ *   Pass in the directory to your Go module.
+ *
+ *   ```ts title="sst.config.ts"
+ *   new sst.aws.Function("MyFunction", {
+ *     runtime: "go",
+ *     handler: "./src"
+ *   });
+ *   ```
+ *
+ *   [Learn more below](#handler).
+ *   </TabItem>
+ *   <TabItem label="Rust">
+ *   Pass in the directory where your Cargo.toml lives.
+ *
+ *   ```ts title="sst.config.ts"
+ *   new sst.aws.Function("MyFunction", {
+ *     runtime: "rust",
+ *     handler: "./crates/api/"
+ *   });
+ *   ```
+ *
+ *   [Learn more below](#handler).
+ *   </TabItem>
+ * </Tabs>
+ *
+ * #### Set additional config
+ *
+ * Pass in additional Lambda config.
+ *
+ * ```ts {3,4} title="sst.config.ts"
+ * new sst.aws.Function("MyFunction", {
+ *   handler: "src/lambda.handler",
+ *   timeout: "3 minutes",
+ *   memory: "1024 MB"
+ * });
+ * ```
+ *
+ * #### Link resources
+ *
+ * [Link resources](/docs/linking/) to the function. This will grant permissions
+ * to the resources and allow you to access it in your handler.
+ *
+ * ```ts {5} title="sst.config.ts"
+ * const bucket = new sst.aws.Bucket("MyBucket");
+ *
+ * new sst.aws.Function("MyFunction", {
+ *   handler: "src/lambda.handler",
+ *   link: [bucket]
+ * });
+ * ```
+ *
+ * You can use the [SDK](/docs/reference/sdk/) to access the linked resources
+ * in your handler.
+ *
+ * <Tabs>
+ *   <TabItem label="Node">
+ *   ```ts title="src/lambda.ts"
+ *   import { Resource } from "sst";
+ *
+ *   console.log(Resource.MyBucket.name);
+ *   ```
+ *   </TabItem>
+ *   <TabItem label="Python">
+ *   ```ts title="functions/src/functions/api.py"
+ *   from sst import Resource
+ *
+ *   def handler(event, context):
+ *       print(Resource.MyBucket.name)
+ *   ```
+ *
+ *   Where the `sst` package can be added to your `pyproject.toml`.
+ *
+ *   ```toml title="functions/pyproject.toml"
+ *   [tool.uv.sources]
+ *   sst = { git = "https://github.com/sst/sst.git", subdirectory = "sdk/python", branch = "dev" }
+ *   ```
+ *   </TabItem>
+ *   <TabItem label="Go">
+ *   ```go title="src/main.go"
+ *   import (
+ *     "github.com/sst/sst/v3/sdk/golang/resource"
+ *   )
+ *
+ *   resource.Get("MyBucket", "name")
+ *   ```
+ *   </TabItem>
+ *   <TabItem label="Rust">
+ *   ```rust title="src/main.rs"
+ *   use sst_sdk::Resource;
+ *   #[derive(serde::Deserialize, Debug)]
+ *   struct Bucket {
+ *      name: String,
+ *   }
+ *
+ *   let resource = Resource::init().unwrap();
+ *   let Bucket { name } = resource.get("Bucket").unwrap();
+ *   ```
+ *   </TabItem>
+ * </Tabs>
+ *
+ * #### Set environment variables
+ *
+ * Set environment variables that you can read in your function. For example, using
+ * `process.env` in your Node.js functions.
+ *
+ * ```ts {4} title="sst.config.ts"
+ * new sst.aws.Function("MyFunction", {
+ *   handler: "src/lambda.handler",
+ *   environment: {
+ *     DEBUG: "true"
+ *   }
+ * });
+ * ```
+ *
+ * #### Enable function URLs
+ *
+ * Enable function URLs to invoke the function over HTTP.
+ *
+ * ```ts {3} title="sst.config.ts"
+ * new sst.aws.Function("MyFunction", {
+ *   handler: "src/lambda.handler",
+ *   url: true
+ * });
+ * ```
+ *
+ * #### Bundling
+ *
+ * Customize how SST uses [esbuild](https://esbuild.github.io/) to bundle your Node.js
+ * functions with the `nodejs` property.
+ *
+ * ```ts title="sst.config.ts" {3-5}
+ * new sst.aws.Function("MyFunction", {
+ *   handler: "src/lambda.handler",
+ *   nodejs: {
+ *     install: ["pg"]
+ *   }
+ * });
+ * ```
+ *
+ * Or override it entirely by passing in your own function `bundle`.
+ */
 export class Image extends Component implements Link.Linkable {
   private constructorName: string;
   private image: Output<PulumiDockerImage>;
