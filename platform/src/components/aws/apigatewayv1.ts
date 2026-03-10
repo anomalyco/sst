fe308015547f1e02c15dd547ea108eb9d39f2f3a
vimtor <vn4varro@gmail.com>
2026-03-10 23:27:38 +0100

    add apigv2 bit

--- platform/src/components/aws/apigatewayv1.ts
+++ platform/src/components/aws/apigatewayv1.ts
@@ -622,6 +622,8 @@ export interface ApiGatewayV1RouteArgs {
   /**
    * Enable streaming for the route.
    *
+   * Streaming is only supported for API Gateway REST APIs, not HTTP APIs.
+   *
    * You'll also need to [wrap your handler](https://docs.aws.amazon.com/lambda/latest/dg/configuration-response-streaming.html) with `awslambda.streamifyResponse` to enable streaming.
    *
    * Check out the [AWS API Gateway V1 streaming](/docs/examples/#aws-api-gateway-v1-streaming) for more
