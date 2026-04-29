5707c4a2c3c778474146789ac8a91df8f26abb7e
vimtor <vn4varro@gmail.com>
2026-04-29 16:19:28 +0200

    simplify llms endpoints by removing internal component filter file

--- www/src/pages/llms-full.txt.ts
+++ www/src/pages/llms-full.txt.ts
@@ -1,13 +1,11 @@
 import { getCollection } from "astro:content";
 import type { APIRoute } from "astro";
 import { cleanMarkdown } from "../util/markdown";
-import { isExcludedFromLlmsFull } from "../util/llms-filter";
-
 export const GET: APIRoute = async () => {
   const docs = await getCollection("docs");
   const filtered = docs
     .filter((doc) => doc.id.startsWith("docs/"))
-    .filter((doc) => !isExcludedFromLlmsFull(doc.id.replace(/\.mdx?$/, "")))
+    .filter((doc) => doc.id.replace(/\.mdx?$/, "") !== "docs/examples")
     .sort((a, b) => a.id.localeCompare(b.id));
 
   const pages = filtered.map((doc) => {
